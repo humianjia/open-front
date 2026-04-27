b1a13c82141afd8b38662b6b5b6370c422d32d97
mianjia.hu <humianjia1995@163.com>
2026-04-27 08:22:56 +0800

    将所有页面favicon改为frontwarsio.png图标

---
+++ update_favicon.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const path = require('path');
+
+const rootDir = __dirname;
+const htmlFiles = [];
+
+function findHtmlFiles(dir) {
+    const items = fs.readdirSync(dir);
+    for (const item of items) {
+        if (item === 'node_modules') continue;
+        const fullPath = path.join(dir, item);
+        const stat = fs.statSync(fullPath);
+        if (stat.isDirectory()) {
+            findHtmlFiles(fullPath);
+        } else if (item.endsWith('.html')) {
+            htmlFiles.push(fullPath);
+        }
+    }
+}
+
+findHtmlFiles(rootDir);
+
+const oldFaviconPattern = /<link rel="icon"[^>]*>/;
+const newFavicon = '<link rel="icon" type="image/png" href="img/icon/frontwarsio.png">';
+
+let updatedCount = 0;
+
+for (const file of htmlFiles) {
+    let content = fs.readFileSync(file, 'utf-8');
+
+    if (oldFaviconPattern.test(content)) {
+        content = content.replace(oldFaviconPattern, newFavicon);
+        fs.writeFileSync(file, content);
+        console.log(`Updated: ${file}`);
+        updatedCount++;
+    }
+}
+
+console.log(`\nTotal updated: ${updatedCount} files`);
