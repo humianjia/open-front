ae46e0b720fb92e20111bfdc98ee1afa73a00bd0
mianjia.hu <humianjia1995@163.com>
2026-04-27 08:29:45 +0800

    修复所有页面的favicon为frontwarsio.png

---
+++ fix_favicon.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const path = require('path');
+
+const rootDir = __dirname;
+const htmlFiles = [];
+
+function findHtmlFiles(dir) {
+    const items = fs.readdirSync(dir);
+    for (const item of items) {
+        if (item === 'node_modules') continue;
+        const fullPath = path.join(dir, item);
+        const stat = fs.statSync(fullPath);
+        if (stat.isDirectory()) {
+            findHtmlFiles(fullPath);
+        } else if (item.endsWith('.html')) {
+            htmlFiles.push(fullPath);
+        }
+    }
+}
+
+findHtmlFiles(rootDir);
+
+const newFavicon = '<link rel="icon" type="image/png" href="img/icon/frontwarsio.png">';
+
+let updatedCount = 0;
+
+for (const file of htmlFiles) {
+    let content = fs.readFileSync(file, 'utf-8');
+    
+    const lines = content.split('\n');
+    let newContent = [];
+    let found = false;
+    
+    for (const line of lines) {
+        if (line.includes('rel="icon"') && !found) {
+            newContent.push(`    ${newFavicon}`);
+            found = true;
+        } else if (!line.includes('rel="icon"')) {
+            newContent.push(line);
+        }
+    }
+    
+    if (found) {
+        fs.writeFileSync(file, newContent.join('\n'));
+        console.log(`Updated: ${file}`);
+        updatedCount++;
+    }
+}
+
+console.log(`\nTotal updated: ${updatedCount} files`);
