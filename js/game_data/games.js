f2cd89be42644a2dea019ba768d2213271c8b7b9
humianjia <humianjia1995@163.com>
2026-05-18 08:25:11 +0800

    1

--- js/game_data/games.js
+++ js/game_data/games.js
@@ -1,83 +1,47 @@
 var gamesData = [
     {
-        "id": "366a198cba9742ad9a755dbef736743c",
-        "name": "Bullet Heroes",
-        "imageUrl": "img/icon/Action/Bullet_Heroes.jpg",
-        "gameType": "Featured",
+        "id": "frontwars io",
+        "name": "Frontwars.io",
+        "imageUrl": "img/icon/frontwarsio.png",
+        "gameType": "Index",
         "rating": "4.8",
-        "description": "Bullet Heroes is a featured team-based browser shooter on open front.",
-        "keywords": "Bullet Heroes, open front, browser shooter, featured game",
-        "link": "Action/Bullet_Heroes.html",
+        "description": "FrontWars.io is a fast-paced multiplayer real-time strategy game where dozens of players battle for map dominance. Conquer territories, build cities, construct ports, and forge alliances. Victory comes when you control 72% of the map! Play for free on browser.",
+        "keywords": "frontwars io, frontwars io game, frontwars io strategy, frontwars io multiplayer, frontwars io online, frontwars io free, frontwars io browser, frontwars io play, frontwars io battle, frontwars io conquest, frontwars io war, frontwars io territory, frontwars io alliance, frontwars io multiplayer strategy, frontwars io .io games, frontwars io strategy game",
+        "link": "index.html",
         "tags": [
-            "bullet heroes",
-            "featured",
-            "shooter",
-            "open front"
-        ],
-        "iframeUrl": "https://html5.gamedistribution.com/366a198cba9742ad9a755dbef736743c/?gd_sdk_referrer_url=https%3A%2F%2Fwww.onlinegames.io%2Fcat-runner%2F"
-    },
-    {
-        "id": "c43c108ebd914fbfa9dddfcbcbadcae2",
-        "name": "GunGame Poligon Battle Royale",
-        "imageUrl": "img/icon/BattleRoyale/GunGame_Poligon_Battle_Royale.jpeg",
-        "gameType": "Battle Royale",
-        "rating": "4.7",
-        "description": "A fast survival shooter built for quick battles.",
-        "keywords": "GunGame Poligon Battle Royale, battle royale, survival, open front",
-        "link": "BattleRoyale/GunGame_Poligon_Battle_Royale.html",
-        "tags": [
-            "battle royale",
-            "survival",
-            "open front"
-        ],
-        "iframeUrl": "https://html5.gamedistribution.com/c43c108ebd914fbfa9dddfcbcbadcae2/?gd_sdk_referrer_url=https%3A%2F%2Fwww.onlinegames.io%2Fcat-runner%2F"
-    },
-    {
-        "id": "090a6a819ccb48b3ad0ea9457ef96770",
-        "name": "Pixel Arena Game FPS",
-        "imageUrl": "img/icon/FPS/Pixel_Arena_Game_FPS.jpeg",
-        "gameType": "FPS",
-        "rating": "4.6",
-        "description": "Pixel Arena Game FPS offers fast multiplayer shooting.",
-        "keywords": "Pixel Arena Game FPS, fps, shooter, open front",
-        "link": "FPS/Pixel_Arena_Game_FPS.html",
-        "tags": [
-            "fps",
-            "shooter",
-            "open front"
-        ],
-        "iframeUrl": "https://html5.gamedistribution.com/090a6a819ccb48b3ad0ea9457ef96770/?gd_sdk_referrer_url=https%3A%2F%2Fwww.onlinegames.io%2Fcat-runner%2F"
-    },
-    {
-        "id": "ed99a63b0a35407792cbd1f93b7b7b83",
-        "name": "ColorWars.io - Conquest Game",
-        "imageUrl": "img/icon/Multiplayer/ColorWars.io_-_Conquest_Game.jpg",
-        "gameType": "Multiplayer",
-        "rating": "4.5",
-        "description": "ColorWars.io is a competitive territory game.",
-        "keywords": "ColorWars.io - Conquest Game, multiplayer, io, open front",
-        "link": "Multiplayer/ColorWars.io_-_Conquest_Game.html",
-        "tags": [
+            "frontwars io",
+            "strategy",
+            "conquest",
             "multiplayer",
-            "io",
-            "open front"
+            "browser game",
+            "io games",
+            "action games",
+            "war games",
+            "online game",
+            "free game",
+            "battle",
+            "competitive strategy",
+            "resource management",
+            "alliance system"
         ],
-        "iframeUrl": "https://html5.gamedistribution.com/ed99a63b0a35407792cbd1f93b7b7b83/?gd_sdk_referrer_url=https%3A%2F%2Fwww.onlinegames.io%2Fcat-runner%2F"
+        "iframeUrl": "https://frontwars.io",
+        "gameplay": "FrontWars.io gameplay revolves around strategic territory expansion. Start with a small base, conquer adjacent territories, balance troops and workers, and build cities, ports, and defense posts. Form alliances, trade resources, and deploy naval forces and missiles to dominate the map.",
+        "gameModes": [
+            "Free For All",
+            "Alliance Mode",
+            "Conquest",
+            "Team Battle"
+        ],
+        "features": [
+            "No download required",
+            "Browser-based multiplayer",
+            "Real-time strategy",
+            "Map conquest gameplay",
+            "Alliance system",
+            "Naval warfare",
+            "Missile system",
+            "Resource management",
+            "Cross-platform play"
+        ]
     },
-    {
-        "id": "8bd8d8dc794c4bf0b3e04e3f8ca4203f",
-        "name": "Sniper Master",
-        "imageUrl": "img/icon/Sniper/Sniper_Master.jpg",
-        "gameType": "Sniper",
-        "rating": "4.7",
-        "description": "Take aim and hit clean shots in Sniper Master.",
-        "keywords": "Sniper Master, sniper, precision, open front",
-        "link": "Sniper/Sniper_Master.html",
-        "tags": [
-            "sniper",
-            "precision",
-            "open front"
-        ],
-        "iframeUrl": "https://html5.gamedistribution.com/8bd8d8dc794c4bf0b3e04e3f8ca4203f/?gd_sdk_referrer_url=https%3A%2F%2Fwww.onlinegames.io%2Fcat-runner%2F"
-    }
-];
+]
